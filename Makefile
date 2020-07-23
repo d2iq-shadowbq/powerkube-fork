90db966b33813e7be48194bf1de3660d5e964a94
d2iq-shadowbq <smacgregor@d2iq.com>
2020-07-23 14:53:14 -0400

    v0.2.1

--- Makefile
+++ Makefile
@@ -1,5 +1,22 @@
-test-upload:
-	python setup.py sdist upload -r pypitest
 
-upload:
-	python setup.py sdist upload -r pypi
+.PHONY: help
+# target: help - Display callable targets
+help:
+	@egrep "^# target:" [Mm]akefile
+
+.PHONY: clean
+# target: clean - Clean all elements except wiping the VIRTENV
+clean:
+	python3 setup.py clean --dist --eggs --build --pycache
+
+.PHONY: upload
+# target: upload - Upload module on PyPi
+upload: clean
+	python3 setup.py clean --dist
+	python3 setup.py sdist bdist_wheel
+	twine upload dist/*
+
+.PHONY: install
+# target: install - Build and Install it locally only
+install: 
+	python3 setup.py install
--- setup.py
+++ setup.py
@@ -6,6 +6,20 @@ try:
 except ImportError:
     from distutils.core import setup
 
+try:
+   from setupext_janitor import janitor
+   CleanCommand = janitor.CleanCommand
+except ImportError:
+   CleanCommand = None
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+cmd_classes = {}
+if CleanCommand is not None:
+   cmd_classes['clean'] = CleanCommand
+
 INSTALL_REQUIRES = [
     'kubernetes-py>=1.10.7.4',
     'powerline-status>=2.7',
@@ -18,17 +32,27 @@ def version():
 # Heavily inspired by the powerline-kubernetes library written by Vincent De Smet
 # (vincent.drl@gmail.com) and located at https://github.com/so0k/powerline-kubernetes
 setup(
+    setup_requires=['setupext_janitor'],
+    cmdclass=cmd_classes,
+    entry_points={
+      # normal parameters, ie. console_scripts[]
+      'distutils.commands': [
+         ' clean = setupext_janitor.janitor:CleanCommand']
+     },
     name='powerkube-fork',
     version=version(),
     description='A powerline segment to show kubernetes context items, with toggling and alert color functionality',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Scott MacGregor',
-    author_email='shadowbq@gmail.com',
+    author_email='smacgregor@d2iq.com',
     url='https://github.com/d2iq-shadowbq/powerkube-fork',
     download_url='https://github.com/d2iq-shadowbq/powerkube-fork/tarball/'+ version(),
+    
     packages=['powerkube_fork'],
     python_requires='>=3.6',
     install_requires=INSTALL_REQUIRES,
-    license='Copyright',
+    license='Apache 2.0',
     keywords=['kubernetes_py','k8s','powerline','powerkube'],
     classifiers=[
         'Programming Language :: Python :: 3',
